191b65d4df71076be1f7b475dea7fbd75f44deea
DidacLinares <didaclinares0@gmail.com>
2025-04-16 10:36:50 +0200

    General stats added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -23,6 +23,19 @@ function Index({data}) {
     <div>
       <h1>General overview</h1>
       <div className="grid-container">
+        <div className="grid-item general-stats">
+            <h2>Project Stats</h2>
+            <ul>
+            <li><strong>Total Members:</strong> {Object.keys(data.commits).filter(user => user !== 'anonymous' && user !== 'total').length}</li>
+            <li><strong>Total Commits:</strong> {data.commits.total}</li>
+            <li><strong>Total Issues:</strong> {data.issues.total}</li>
+            <li><strong>Total Pull Requests:</strong> {data.pull_requests.total}</li>
+            <li><strong>Total Additions:</strong> {data.modified_lines.total.additions}</li>
+            <li><strong>Total Deletions:</strong> {data.modified_lines.total.deletions}</li>
+            <li><strong>Total Modifications:</strong> {data.modified_lines.total.modified}</li>
+            <li><strong>Total Lines of code:</strong> {data.modified_lines.total.additions - data.modified_lines.total.deletions}</li>
+            </ul>
+          </div>
         <div className="grid-item">
           <PieChart 
             title="Pull requests state summary" 
